8f5e48bf88d579bc468dec1e26ad1e6f6dd5d6f0
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2023-09-16 10:16:59 +0700

    create project, structure.

---
+++ src/main/java/vn/edu/iuh/fit/www_lab_week2/models/Employee.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_lab_week2.models;
+
+public class Employee {
+    private String emp_id;
+    private String full_name;
+    private String dob;
+    private String email;
+    private String phone;
+    private String address;
+    private String status;
+
+
+}
