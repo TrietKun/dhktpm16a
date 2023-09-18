653bdc74da9b242b88f62c43a3dd5cb18026cae1
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2023-09-18 17:13:32 +0700

    create project, structure.

--- src/main/java/vn/edu/iuh/fit/www_lab_week2/models/Order_detail.java
+++ src/main/java/vn/edu/iuh/fit/www_lab_week2/models/Order_detail.java
@@ -4,27 +4,24 @@ package vn.edu.iuh.fit.www_lab_week2.models;
 //ngồi làm nhưng không thoát GitHub trong intelliJ máy  em ngồi
 //và  em không để ý nên đã commit bài nhầm vào tài khoản của bạn  chứ không phải em copy bài ạ!
 public class Order_detail {
-    private long id;
-    private String product_id;
+    private long order_id;
+    private long product_id;
     private int quantity;
     private double price;
     private String note;
 
-    public long getId() {
-        return id;
+    public long getOrder_id() {
+        return order_id;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setOrder_id(long order_id) {
+        this.order_id = order_id;
     }
 
-    public String getProduct_id() {
-        return product_id;
-    }
-
-    public void setProduct_id(String product_id) {
+    public void setProduct_id(long product_id) {
         this.product_id = product_id;
     }
+
 
     public int getQuantity() {
         return quantity;
@@ -53,8 +50,8 @@ public class Order_detail {
     public Order_detail() {
     }
 
-    public Order_detail(long id, String product_id, int quantity, double price, String note) {
-        this.id = id;
+    public Order_detail(long order_id, long product_id, int quantity, double price, String note) {
+        this.order_id = order_id;
         this.product_id = product_id;
         this.quantity = quantity;
         this.price = price;
@@ -64,8 +61,8 @@ public class Order_detail {
     @Override
     public String toString() {
         return "Order_detail{" +
-                "id=" + id +
-                ", product_id='" + product_id + '\'' +
+                "order_id=" + order_id +
+                ", product_id=" + product_id +
                 ", quantity=" + quantity +
                 ", price=" + price +
                 ", note='" + note + '\'' +
